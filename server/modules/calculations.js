903dc03e4a3b2dc74690e490b12f1dd2ab4cdcb8
Jeremy Hanson <rockbasser88@hotmail.com>
2022-12-10 12:57:59 -0600

    changed file structure to run correctly

---
+++ server/modules/calculations.js
@@ -0,0 +1,8 @@
+let calculations = [
+    {
+        numOne: 1,
+        numTwo: 1,
+        operator: '+',
+        result: 2
+    }
+];
--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-// import express library
 const express = require('express');
-// import body-parser
-const bodyParser = require('body-parser');
-// make an instance of an express server
+const bodyParser = require('body-parser')
 const app = express();
-// tell server to run on port 5000
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
-// tells server where to find static assets (HTML, CSS, Client JS)
+// Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// start the server
+
+
 app.listen(PORT, () => {
     console.log ('Server is running on port', PORT)
   })
